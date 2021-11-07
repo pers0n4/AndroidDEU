36485d0afc9fb4e761ed5fd3757787e68e73d7a5
Dong-Young Kim <31337.persona@gmail.com>
2021-11-07 20:10:10 +0900

    feat: add naver map activity

---
+++ app/src/main/java/kr/ac/deu/se/example/NaverMapActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.deu.se.example;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NaverMapActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_naver_map);
+  }
+}
