8a92043f1888f5a29fb3ad7eadd17364b0d3e5ae
Dong-Young Kim <31337.persona@gmail.com>
2021-11-11 14:43:09 +0900

    feat: implement dynamically change list item feature

--- app/src/main/java/kr/ac/deu/se/example/AdapterViewDemoActivity.kt
+++ app/src/main/java/kr/ac/deu/se/example/AdapterViewDemoActivity.kt
@@ -2,8 +2,9 @@ package kr.ac.deu.se.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
 import android.widget.Toast
 
@@ -12,11 +13,25 @@ class AdapterViewDemoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adapter_view_demo)
 
-        val listItems = arrayOf("x", "y", "z")
+        val listItems = mutableListOf("x", "y", "z")
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
         val listView = findViewById<ListView>(R.id.listView)
         listView.adapter = adapter
+
+        val editText = findViewById<EditText>(R.id.editText)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            listItems.add(editText.text.toString())
+            adapter.notifyDataSetChanged()
+        }
+
+        listView.setOnItemLongClickListener { parent, view, position, id ->
+            listItems.removeAt(position)
+            adapter.notifyDataSetChanged()
+            return@setOnItemLongClickListener false
+        }
 
         listView.setOnItemClickListener { parent, view, position, id ->
             Toast.makeText(applicationContext, listItems[position], Toast.LENGTH_SHORT).show()
